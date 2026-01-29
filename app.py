98d5fe16d84fc627081eac56c0b4c237ca978523
leolove60XXX <leolove81103@gmail.com>
2026-01-29 14:46:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -118,9 +118,9 @@ if st.session_state.submitted:
                                         return parts[1]
                     return ""
                 except Exception as e:
-        # 發生錯誤時顯示在後台或回傳空白
-                print(f"抓取名稱出錯: {e}")
-                return ""
+                    # 發生錯誤時顯示在後台或回傳空白
+                    print(f"抓取名稱出錯: {e}")
+                    return ""
 
 # 執行抓取
 comp_name = get_tw_stock_name(base_id)
